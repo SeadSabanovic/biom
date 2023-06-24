dea9c081999a0aa25fcce6f470712e13db4ddeec
Sead Sabanovic <s.sabanovic@agentur-loop.com>
2023-06-24 13:47:58 +0200

    WIP Testimonials

--- src/app/components/home/section-testimonials/section-testimonials.component.ts
+++ src/app/components/home/section-testimonials/section-testimonials.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { Testimonial } from 'src/app/interfaces/testimonial';
 
 @Component({
   selector: 'section-testimonials',
   templateUrl: './section-testimonials.component.html',
-  styleUrls: ['./section-testimonials.component.scss']
+  styleUrls: ['./section-testimonials.component.scss'],
 })
 export class SectionTestimonialsComponent implements OnInit {
+  testimonials: Testimonial[] = [
+    {
+      author: 'RACHAEL H.',
+      testimonial:
+        'Cleaning and sanitizing are now more important than ever and these wipes make it so easy. They’re great because they don’t dry my skin out and I love knowing that buying their products are better for the environment.',
+    },
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
