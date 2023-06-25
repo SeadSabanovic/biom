8584e71538a2d692347e3e00a5da2c7fbf13c091
Sead Sabanovic <s.sabanovic@agentur-loop.com>
2023-06-25 19:39:38 +0200

    light button info section

--- src/app/components/UI/button/button.component.ts
+++ src/app/components/UI/button/button.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class ButtonComponent implements OnInit {
   @Input() label = 'Input a label';
-  @Input() color = 'light';
+  @Input() style = 'light';
   constructor() {}
 
   ngOnInit() {}
