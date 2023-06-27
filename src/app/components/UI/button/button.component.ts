26ce53ccfa2759e9e8aadd9d907c710e56a532d5
Sead Sabanovic <s.sabanovic@agentur-loop.com>
2023-06-27 03:50:22 +0200

    Responsive Additional Sec + outline button style

--- src/app/components/UI/button/button.component.ts
+++ src/app/components/UI/button/button.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit, Input } from '@angular/core';
 export class ButtonComponent implements OnInit {
   @Input() label = 'Input a label';
   @Input() style = 'light';
+  @Input() icon = '';
   constructor() {}
 
   ngOnInit() {}
