f4340102acbf3616a419ef1271b0faf87c815aa2
s.sabanovic <s.sabanovic@agentur-loop.com>
2023-06-29 18:35:04 +0200

    Added vh calculation

--- src/app/services/resize.service.ts
+++ src/app/services/resize.service.ts
@@ -9,10 +9,17 @@ export class ResizeService {
   private resizeObservable: Observable<Event>;
 
   constructor() {
+    this.setWindowHeight();
     this.resizeObservable = fromEvent(window, 'resize').pipe(debounceTime(100));
   }
 
   get resize$(): Observable<Event> {
+    this.setWindowHeight();
     return this.resizeObservable;
+  }
+
+  setWindowHeight() {
+    const vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
   }
 }
